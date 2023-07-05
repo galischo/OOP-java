9468f0fb4786174085931750a98e42b0ce5fa426
galischo <avig4553@gmail.com>
2023-07-05 13:04:07 +0300

    my first

--- ObjectOriented.java
+++ ObjectOriented.java
@@ -13,13 +13,22 @@ public class ObjectOriented {
 
     
     public static void main(String[] args) {
+        //cat object
        Cat c=new Cat(true,true,Animal.MOOD_HAPPY);
+       //dog object
        Dog d=new Dog(true,true,Animal.MOOD_SCARE);
+       //frog object
        Frog f=new Frog(false,false,Animal.MOOD_HAPPY);
+       //try the funtions
        c.sayHello();
        d.sayHello(Animal.MOOD_HAPPY);
        System.out.println(f.isCarnivious());
        System.out.println(f.hasGills());
+       //polymorphism 
+       Animal a=new Frog(false,true,Animal.MOOD_SCARE);
+       System.out.println(a.isMammals());
+       a.sayHello(Animal.MOOD_HAPPY);
+       System.out.println(((Frog)(a)).hasLaysEggs());
        
     }
     
